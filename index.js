374dcff6a9afad1220182adc5c7d7b833bef802f
David Zemina <35789015+dczemina@users.noreply.github.com>
2023-10-16 15:03:29 -0400

    Add comments to operate function

--- index.js
+++ index.js
@@ -22,6 +22,14 @@ let operator;
 let number2;
 
 // UI Functions
+
+/**
+ * 
+ * @param {string} op the mathematical operator
+ * @param {number} num1 the first number in the operation
+ * @param {number} num2 the second number in the operation
+ * @returns the result of the operation using the two numbers as input. num1 is the left-hand side of operations.
+ */
 function operate(op, num1, num2) {
     if (op === '+' || op.toLowerCase() === 'add')
         return add(num1, num2);
