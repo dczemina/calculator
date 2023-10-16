c44a81de7cdeeede9df71ccc7e711f1e3b3cc7e9
David Zemina <35789015+dczemina@users.noreply.github.com>
2023-10-15 20:22:03 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+// Math Functions
+
+function add(a, b) {
+    console.log(arguments);
+    return a + b;
+}
+function subtract(a, b) {
+    console.log(arguments);
+    return a - b;
+}
+function multiply(a, b) {
+    console.log(arguments);
+    return a * b;
+}
+function divide(a, b) {
+    console.log(arguments);
+    return a / b;
+}
