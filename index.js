c4f03270a4456da4e28b1cc360c738b2a50c2dea
David Zemina <35789015+dczemina@users.noreply.github.com>
2023-10-16 15:02:02 -0400

    Implement operate function

--- index.js
+++ index.js
@@ -22,6 +22,14 @@ let operator;
 let number2;
 
 // UI Functions
-function operate(num1, op, num2) {
-    
+function operate(op, num1, num2) {
+    if (op === '+' || op.toLowerCase() === 'add')
+        return add(num1, num2);
+    if (op === '-' || op.toLowerCase() === 'subtract')
+        return subtract(num1, num2);
+    if (op === '*' || op.toLowerCase() === 'multiply')
+        return multiply(num1, num2);
+    if (op === '/' || op.toLowerCase() === 'divide')
+        return divide(num1, num2);
+    return 'Invalid operation'
 }
